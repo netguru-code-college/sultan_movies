835774da1fa01ae77e7ff7e7c8a1cf4378cbc879
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2018-08-22 15:45:37 +0200

    add association habtm to actor and cast

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,6 @@
 class Actor < ApplicationRecord
+  has_and_belongs_to_many :casts
+
   validates :first_name, :last_name,
     presence: true,
     format: { with: /\A[a-zA-Z]+\z/, message: 'only allows letters' }
--- app/models/cast.rb
+++ app/models/cast.rb
@@ -1,5 +1,6 @@
 class Cast < ApplicationRecord
   belongs_to :movie
+  has_and_belongs_to_many :actors
 
   validates :movie, presence: true
 end
