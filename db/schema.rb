aae49cef8526df3513617bd3b1fc537751e22552
Aleksandra Kurdo <aleksandra.kurdo@netguru.pl>
2018-08-22 14:38:28 +0200

    add movie table

---
+++ db/schema.rb
@@ -0,0 +1,13 @@
+ActiveRecord::Schema.define(version: 2018_08_22_122537) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
